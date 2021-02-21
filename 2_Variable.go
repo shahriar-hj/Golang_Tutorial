69db5e0cdea5c5e6a09f3d23a970fd7e0a2ca515
Shahriar <shahriarhajiabbasi@gmail.com>
2021-02-21 19:34:18 +0330

    first 2 Section of Tutorial
    LEarning Go is Cool and I have just Start it

---
+++ 2_Variable.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	name = "Hello world"
+	fmt.Println(name)
+
+	var number uint32
+	number = 23
+	fmt.Println(number, name)
+}
+
+// Learn different Data Types and Variables
