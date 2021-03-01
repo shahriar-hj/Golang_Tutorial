9941afbed69ccdcb8334a789b6d284d7d2db860d
Shahriar <shahriarhajiabbasi@gmail.com>
2021-03-01 14:06:28 +0330

    Update 9_if_else.go
    elseif

--- 9_if_else.go
+++ 9_if_else.go
@@ -3,14 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	age  := 13
+	age := 19
 	fmt.Println("you are: ", age)
 	if age >= 18 {
-		fmt.Println("you can ride alone ")
-	}else{
-		fmt.Println("Sorry you can not ride it ")
+		fmt.Println("you can ride alone! ")
+	} else if age >= 14 {
+		fmt.Println("you can ride with parent! ")
+
+	} else {
+		fmt.Println("Sorry, you can not Ride!")
 		fmt.Printf("Wait %d years \n", 18-age)
+
 	}
-	
+
 	fmt.Println("enjoy the Day ! ")
 }
